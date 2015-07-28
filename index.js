9f054e9122ec355e800c18781742882627a8fe6c
Christopher Blum <christopher.blum@spellboy.com>
2015-07-28 14:11:58 +0200

    status and update calls

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var express = require('express')
+var express = require('express');
+var fs = require('fs');
 var app = express();
+var storagePath = "/data/toilet_status.txt";
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('*', function(req, res){
-  console.log(req)
-  res.send('Hello World.');
+app.get('/update', function(req, res) {
+  console.log(new Date().toString(), "Received update:", res.params.status);
+  fs.writeFileSync(storagePath, JSON.stringify({ status: res.params.status }));
+  res.json({ success: true });
 });
 
-// app.get('/', function(request, response) {
-//   response.send('Toilet is occupied!')
-// })
+app.get('/status', function(req, res) {
+  var data = fs.readFileSync(storagePath);
+  res.json(JSON.parse(data));
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
