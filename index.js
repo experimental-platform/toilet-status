5b30a7d818d404a068c3463b6997f0cf595c8500
Christopher Blum <christopher.blum@spellboy.com>
2015-07-28 13:58:58 +0200

    Catch all requests

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('*', function(req, res){
+  console.log(req)
+  response.send('Hello World.');
+});
+
+// app.get('/', function(request, response) {
+//   response.send('Toilet is occupied!')
+// })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
